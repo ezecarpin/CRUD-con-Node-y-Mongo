5bb1b40177e159ba22db8b300faea2d511cdfa33
ezecarpin <ezecarpin@hotmail.com>
2021-05-29 17:57:18 -0300

    variables de entorno

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
 
+require('dotenv').config()
 
-// Motor de plantilla
-app.set("view engine", "ejs")
-app.set("views", __dirname + "/views")
+const port = process.env.PORT || 3000
+
+//conexion a base de datos
+const mongoose = require('mongoose')
+
+const uri = `mongodb+srv://eze:${process.env.PASSWORD}@cluster0.z9vwg.mongodb.net/veterinaria?retryWrites=true&w=majority`
+
+mongoose.connect(uri, 
+    {useNewUrlParser: true, useUnifiedTopology: true})
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+      console.log("panfleto") 
+    });
+
+// motor de plantillas
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
 
 app.use(express.static(__dirname + "/public"))
 
-app.get ("/", (req, res) => {
-    res.render("index", { titulo: "inicio con EJS"})
-})
-
-app.get ("/servicios", (req, res) => {
-    res.render("servicios", { titulo: "Pagina de nosotros"})
-})
+//Rutas web
+app.use('/', require('./router/RutasWeb'))
+app.use('/mascotas', require('./router/Mascotas'))
 
 app.use((req, res, next) => {
-    res.status(404).render("404", { titulo: "Pagina 404"})
+    res.status(404).render("404", {
+        titulo: "404",
+        descripcion: "Título del sitio web"
+    })
 })
 
-app.listen(PORT, () => {
-    console.log(`server funcionando en ${PORT}`)
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port)
 })
